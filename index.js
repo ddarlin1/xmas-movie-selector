d62fca4b4d5caa6db732c35b6a8e837bccbe00a9
darling_d <ddarling212@gmail.com>
2021-12-22 21:28:07 -0500

    Added options and customized css

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const ageSelector = document.getElementById("age-selector")
-const genreSelector = document.getElementById("genre-selector")
+const ageSelector = document.getElementById("age-selector");
+const genreSelector = document.getElementById("genre-selector");
 
-const btn = document.getElementById("btn")
-const suggestedMovie = document.getElementById("suggested-movie")
+const btn = document.getElementById("btn");
+const suggestedMovie = document.getElementById("suggested-movie");
+
+btn.addEventListener("click", select);
 
 const moviesArr = [
     {
@@ -21,9 +23,49 @@ const moviesArr = [
         genre: "Action"
     },
     {
-        name: "Shrek",
+        name: "The Nightmare Before Christmas",
         age: "PG",
         genre: "Romance"
+    },
+    {
+        name: "The Night Before",
+        age: "18+",
+        genre: "Comedy"
+    },
+    {
+        name: "Home Alone",
+        age: "PG",
+        genre: "Comedy"
+    },
+    {
+        name: "Carol",
+        age: "18+",
+        genre: "Drama"
+    },
+    {
+        name: "The Muppet Christmas Carol",
+        age: "PG",
+        genre: "Drama"
+    },
+    {
+        name: "It's a Wonderful Life",
+        age: "18+",
+        genre: "Classic"
+    },
+    {
+        name: "How the Grinch Stole Christmas",
+        age: "PG",
+        genre: "Classic"
+    },
+    {
+        name: "Black Christmas",
+        age: "18+",
+        genre: "Horror"
+    },
+    {
+        name: "Gremlins",
+        age: "PG",
+        genre: "Horror"
     }
 ]
 
@@ -31,6 +73,18 @@ const moviesArr = [
 // - Write a function to select a suitable movie based on the age group and genre provided.
 //  - Display it in the suggested-movie paragraph when the button is clicked.
 
+function select() {
+    selectedAge = ageSelector.value;
+    selectedGenre = genreSelector.value;
+    for (let i = 0; i < moviesArr.length; i++) {
+        if (moviesArr[i].age === selectedAge && moviesArr[i].genre === selectedGenre) {
+            suggestedMovie.innerText = moviesArr[i].name;
+        }
+    }
+}
+
+select();
+
 // Stretch goals: 
 // - Have the function run on each change of the <select> tags.
-// - Add more movies/complexity - for example black and white vs color, preferred actors, etc. 
+document.body.addEventListener("change", select);
